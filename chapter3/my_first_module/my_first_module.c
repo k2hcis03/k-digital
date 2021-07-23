a4446efdacc991b90a6d83e096da50cf34e23fd3
k2hcis03 <42025932+k2hcis03@users.noreply.github.com>
2021-07-23 18:16:29 +0900

    Add files via upload

---
+++ chapter3/my_first_module/my_first_module.c
@@ -0,0 +1,26 @@
+/*
+ * my_first_module.c
+ *
+ *  Created on: 2021. 1. 18.
+ *      Author: k2h
+ */
+
+#include <linux/module.h>
+
+static int __init hello_init(void)
+{
+	pr_info("Hello world init\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Hello world exit\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kwang Hyuk Ko");
+MODULE_DESCRIPTION("This is a simple init/exit module test");
