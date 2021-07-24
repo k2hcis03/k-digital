5c5e8f552ca7fe251040f53fa33849ae659433c6
k2hcis03 <42025932+k2hcis03@users.noreply.github.com>
2021-07-24 15:40:09 +0900

    Add files via upload

--- chapter8/recipe8-1/recipe8-1.c
+++ chapter8/recipe8-1/recipe8-1.c
@@ -10,52 +10,54 @@
 #include <linux/gpio/consumer.h>
 #include <linux/property.h>
 #include <linux/interrupt.h>
+#include <linux/timer.h>
+
+#include <linux/hrtimer.h>
+#include <linux/ktime.h>
 
 #define DEVICE_NAME 		"recipedev"
+#define US_TO_NS(x) 		(x * 1000L)
 
-struct recipe10{
+struct recipe8_1{
 	struct platform_device *pdev;
 	struct miscdevice recipe_miscdevice;
-	struct gpio_desc *key_int;
-	int irq;
-	struct tasklet_struct tlet;
-	wait_queue_head_t wait_queue;
-	int flag;
-	unsigned char value;
+	struct hrtimer recipe8_1_timer;
+	int timer_period;
+	struct gpio_desc *gpio;
 };
-static char *KEYS_NAME = "RECIPE10_INT";
 
-static irqreturn_t recipe10_isr(int irq, void *data)
+enum hrtimer_restart recipe8_1_hr_timer(struct hrtimer *t)
 {
-	struct recipe10 *priv = data;
+	static int toggle = 0;
+
+	struct recipe8_1 *recipe_private = from_timer(recipe_private, t, recipe8_1_timer);
 	struct device *dev;
+	ktime_t ktime;
 
-	dev = &priv->pdev->dev;
-	dev_info(dev, "recipe10_isr is called & irq number is %d\n", priv->irq);
+	dev = &recipe_private->pdev->dev;
+	dev_info(dev, "recipe8_1_hr_timer enter %d\n", recipe_private->timer_period);
 
-	tasklet_hi_schedule(&priv->tlet);
-
-	return IRQ_HANDLED;
-}
-
-void recipe10_tasklet_fn(unsigned long data)
-{
-	struct recipe10 *priv = (struct recipe10 *)data;
-	struct device *dev;
-	dev = &priv->pdev->dev;
-	dev_info(dev, "recipe10_tasklet_fn is called\n");
-
-	if(priv->flag == 0){
-		priv->flag = 1;
-		wake_up_interruptible(&priv->wait_queue);
+	if(toggle == 0){
+		gpiod_set_value(recipe_private->gpio, 1);
+		toggle = 1;
+	}else{
+		gpiod_set_value(recipe_private->gpio, 0);
+		toggle = 0;
 	}
+
+	ktime = ktime_set(0, US_TO_NS(recipe_private->timer_period));
+	hrtimer_forward(&recipe_private->recipe8_1_timer,
+			hrtimer_cb_get_time(&recipe_private->recipe8_1_timer), ktime);
+
+	return HRTIMER_RESTART;
+//	return HRTIMER_NORESTART;
 }
 
 static int recipe_open(struct inode *inode, struct file *file)
 {
-	struct recipe10 * recipe_private;
+	struct recipe8_1 * recipe_private;
 	struct device *dev;
-	recipe_private = container_of(file->private_data, struct recipe10, recipe_miscdevice);
+	recipe_private = container_of(file->private_data, struct recipe8_1, recipe_miscdevice);
 	dev = &recipe_private->pdev->dev;
 	dev_info(dev, "recipe_dev_open() is called.\n");
 	return 0;
@@ -63,55 +65,20 @@ static int recipe_open(struct inode *inode, struct file *file)
 
 static int recipe_close(struct inode *inode, struct file *file)
 {
-	struct recipe10 * recipe_private;
+	struct recipe8_1 * recipe_private;
 	struct device *dev;
-	recipe_private = container_of(file->private_data, struct recipe10, recipe_miscdevice);
+	recipe_private = container_of(file->private_data, struct recipe8_1, recipe_miscdevice);
 	dev = &recipe_private->pdev->dev;
 
 	dev_info(dev, "recipe_dev_close() is called.\n");
 	return 0;
 }
 
-
-static int recipe_read(struct file *file, char __user *buff,
-	               size_t count, loff_t *off)
-{
-	struct recipe10 * recipe_private;
-	struct device *dev;
-	int ret;
-	char *data;
-
-	recipe_private = container_of(file->private_data, struct recipe10,
-			recipe_miscdevice);
-	dev = &recipe_private->pdev->dev;
-
-	dev_info(dev, "recipe_read entered\n");
-
-	recipe_private->flag = 0;
-	ret = wait_event_interruptible(recipe_private->wait_queue, recipe_private->flag != 0);
-	if(ret)
-		return ret;
-	dev_info(dev, "recipe_read is wakened\n");
-
-	data = devm_kzalloc(dev, count, GFP_KERNEL);
-	if(!data){
-		dev_info(dev, "failed memory allocation");
-		return -ENOMEM;
-	}
-	recipe_private->value += 1;
-
-	memset(data, recipe_private->value, count);
-	if(copy_to_user(buff, data, count)){
-		return -EFAULT;
-	}
-	*off+=count;
-
-	return count;
-}
 static const struct file_operations recipe_fops = {
 	.owner = THIS_MODULE,
 	.open = recipe_open,
-	.read = recipe_read,
+//	.read = recipe_read,
+//	.unlocked_ioctl = recipe_ioctl,
 	.release = recipe_close,
 };
 
@@ -119,40 +86,19 @@ static int recipe_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct device *dev = &pdev->dev;
-	struct recipe10 *recipe_private;
+	struct recipe8_1 *recipe_private;
+	ktime_t ktime;
 
 	dev_info(dev, "recipe_probe() function is called.\n");
 
-	recipe_private = devm_kzalloc(&pdev->dev, sizeof(struct recipe10), GFP_KERNEL);
+	recipe_private = devm_kzalloc(&pdev->dev, sizeof(struct recipe8_1), GFP_KERNEL);
 	recipe_private->pdev = pdev;
+	recipe_private->timer_period = 200;
 
-	recipe_private->key_int = devm_gpiod_get(dev, NULL, GPIOD_IN);
-	if (IS_ERR(recipe_private->key_int)) {
-			dev_err(dev, "gpio get failed\n");
-			return PTR_ERR(recipe_private->key_int);
-	}
-	recipe_private->irq = gpiod_to_irq(recipe_private->key_int);
-	if (recipe_private->irq < 0)
-		return recipe_private->irq;
+	ktime = ktime_set( 0, US_TO_NS(recipe_private->timer_period));
+	hrtimer_init(&recipe_private->recipe8_1_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL );
+	recipe_private->recipe8_1_timer.function = &recipe8_1_hr_timer;
 
-	dev_info(dev, "The IRQ number is: %d\n", recipe_private->irq);
-
-	ret = devm_request_threaded_irq(dev, recipe_private->irq, recipe10_isr,
-						NULL, IRQF_TRIGGER_FALLING | IRQF_SHARED,
-						KEYS_NAME, recipe_private);
-	if (ret) {
-		dev_err(dev, "Failed to request interrupt %d, error %d\n", recipe_private->irq, ret);
-		return ret;
-	}
-	/* Init the wait queue head */
-	init_waitqueue_head(&recipe_private->wait_queue);
-	recipe_private->flag = 0;
-	recipe_private->value = 0;
-
-	/* Init tasklet */
-	tasklet_init(&recipe_private->tlet, recipe10_tasklet_fn, (unsigned long)recipe_private);
-
-	dev_info(dev, "gpio initial is completed\n");
 	platform_set_drvdata(pdev, recipe_private);
 
 	recipe_private->recipe_miscdevice.name = DEVICE_NAME;
@@ -160,8 +106,18 @@ static int recipe_probe(struct platform_device *pdev)
 	recipe_private->recipe_miscdevice.fops = &recipe_fops;
 	recipe_private->recipe_miscdevice.mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 
+	recipe_private->gpio = devm_gpiod_get(dev, NULL, GPIOD_OUT_LOW);
+	if (IS_ERR(recipe_private->gpio)) {
+			dev_err(dev, "gpio get failed\n");
+			return PTR_ERR(recipe_private->gpio);
+	}
+	gpiod_direction_output(recipe_private->gpio, 0);
+	gpiod_set_value(recipe_private->gpio, 1);
+
 	ret = misc_register(&recipe_private->recipe_miscdevice);
 	if (ret) return ret; /* misc_register returns 0 if success */
+
+	hrtimer_start(&recipe_private->recipe8_1_timer, ktime, HRTIMER_MODE_REL );
 
 	return 0;
 }
@@ -170,15 +126,22 @@ static int recipe_probe(struct platform_device *pdev)
 static int __exit recipe_remove(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct recipe10 *recipe_private = platform_get_drvdata(pdev);
+	struct recipe8_1 *recipe_private = platform_get_drvdata(pdev);
+	int ret;
+
 	dev_info(dev, "recipe_remove() function is called.\n");
+
+	ret = hrtimer_cancel(&recipe_private->recipe8_1_timer);
+	if(ret){
+		dev_info(dev, "The timer was still in use.");
+	}
 	misc_deregister(&recipe_private->recipe_miscdevice);
 	return 0;
 }
 
 /* Declare a list of devices supported by the driver */
 static const struct of_device_id recipe_of_ids[] = {
-	{ .compatible = "brcm,recipe10"},
+	{ .compatible = "brcm,recipe8-1"},
 	{},
 };
 
@@ -189,7 +152,7 @@ static struct platform_driver recipe_platform_driver = {
 	.probe = recipe_probe,
 	.remove = recipe_remove,
 	.driver = {
-		.name = "recipe10",
+		.name = "recipe8-1",
 		.of_match_table = recipe_of_ids,
 		.owner = THIS_MODULE,
 	}
@@ -200,4 +163,4 @@ module_platform_driver(recipe_platform_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kwang Hyuk Ko");
-MODULE_DESCRIPTION("This is a platform & interrupt & tasklet module ");
+MODULE_DESCRIPTION("This is a platform & high resolution timer module ");
